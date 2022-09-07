fe287179194791ace19036b97a02e0a3a8acb832
SamueleLettieri <samuele2073@gmail.com>
2022-09-07 19:54:36 +0200

    fine footer

--- laravel-comics/resources/views/includes/footer.blade.php
+++ laravel-comics/resources/views/includes/footer.blade.php
@@ -1,1 +1,17 @@
-<h3>dshgxhjskx</h3>
+<footer class="p-5">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <div class="p-2 ms_border-button"><a class="ms_link" href="#">SIGN-UP NOW!</a></div>
+            <div>
+                <h3>
+                    <span class="ms_color me-3">FOLLOW US </span>
+                    <a class="ms-3" href="#"><img src="{{asset('assets/footer-facebook.png')}}" alt=""></a>
+                    <a class="ms-3" href="#"><img src="{{asset('assets/footer-twitter.png')}}" alt=""></a>
+                    <a class="ms-3" href="#"><img src="{{asset('assets/footer-youtube.png')}}" alt=""></a>
+                    <a class="ms-3" href="#"><img src="{{asset('assets/footer-pinterest.png')}}" alt=""></a>
+                    <a class="ms-3" href="#"><img src="{{asset('assets/footer-periscope.png')}}" alt=""></a>
+                </h3>
+            </div>
+        </div>
+    </div>
+  </footer>
